04617708e6df94b01099358160e80cad91e4cfd0
Marvin <mdtensuan_dev@protonmail.com>
2021-12-07 19:32:56 +0800

    Added day 7 files.

---
+++ submissions/day7/day7_p2.py
@@ -0,0 +1,22 @@
+
+with open('inputs/day7.txt') as file:
+    txt = file.read()
+    data = [*map(int,txt.split(','))]
+
+
+targets = list(range(
+    min(data),
+    max(data) + 1
+))
+
+def consumption(src, dest):
+    n = abs(dest - src)
+    return n * (n + 1) / 2
+
+
+minima = [
+    sum([consumption(x,y) for x, y in zip(data, [target]*len(data))])
+    for target in targets
+]
+
+print(min(minima))
